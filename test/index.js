7fa394aad14710fc29892d7d3a06b57cac1e2512
Dukky <alex.jieying.bloom@gmail.com>
2023-04-01 01:25:04 -0400

    test website discord

---
+++ test/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { port } = require('./config.json');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.sendFile('index.html', { root: '.' });
+});
+
+app.get('/reminders', (request, response) => {
+    return response.sendFile('reminders.html', { root: '.' });
+});
+
+app.get('/calendar', (request, response) => {
+    return response.sendFile('calendar.html', { root: '.' });
+});
+
+app.get('/donate', (request, response) => {
+    return response.sendFile('donate.html', { root: '.' });
+});
+
+app.get('/login', (request, response) => {
+    return response.sendFile('login.html', { root: '.' });
+});
+
+app.get('/setup', (request, response) => {
+    return response.sendFile('setup.html', { root: '.' });
+});
+
+app.get('/assets/donate', (request, response) => {
+    return response.sendFile('styles/donate.css', { root: '.' });
+});
+
+app.get('/assets/index', (request, response) => {
+    return response.sendFile('styles/index.css', { root: '.' });
+});
+
+app.get('/assets/main', (request, response) => {
+    return response.sendFile('styles/main.css', { root: '.' });
+});
+
+app.get('/assets/reminders', (request, response) => {
+    return response.sendFile('styles/reminders.css', { root: '.' });
+});
+
+app.get('/assets/background', (request, response) => {
+    return response.sendFile('assets/badBackground.png', { root: '.' });
+});
+
+app.get('/assets/main_script', (request, response) => {
+    return response.sendFile('scripts/main.js', { root: '.' });
+});
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
---
+++ test/scripts/main.js
@@ -0,0 +1,10 @@
+let usersNumber = 2;
+document.getElementById("users-number").innerHTML = usersNumber;
+
+let serversNumber = 0;
+document.getElementById("servers-number").innerHTML = serversNumber;
+
+let patrons = "Gerald";
+let plug = "twitch.tv/geraldTV";
+document.getElementById("patrons").innerHTML = patrons + " " + plug;
+document.getElementById("patrons").href = plug;
