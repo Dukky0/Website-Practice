3c647505f6c4577dff04e221c002b27c1739e934
Dukky <alex.jieying.bloom@gmail.com>
2023-04-05 19:36:35 -0400

    Update bot.js

--- Timekeeper Bot/bot.js
+++ Timekeeper Bot/bot.js
@@ -10,4 +10,4 @@ const client = new Client({
 client.once("ready", () => {
     console.log("BOT IS ONLINE"); //message when bot is online
 })
-client.login("MTA5MDQ3MDcwMDcwNTM5ODkzNw.G8pZtw.XxiB4MU0dMgE7PfJAWXilOUba47WgfIG-qxTfA");
+client.login("");
